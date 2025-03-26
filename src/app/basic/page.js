92b7459a59fc3a88411db748387ead31095dfc2b
Randy Oest <oest@amazingrando.com>
2025-03-26 14:43:07 -0400

    wip

--- src/app/basic/page.js
+++ src/app/basic/page.js
@@ -1,7 +1,7 @@
 import Banner from "@/components/banner";
 import Header from "@/components/header";
 import Program from "@/sections/program";
-
+import Footer from "@/components/footer";
 export default function Basic() {
 
   return (
@@ -9,6 +9,7 @@ export default function Basic() {
       <Header/>
       <Program/>
       <Banner/>
+      <Footer/>
     </div>
   );
 }
