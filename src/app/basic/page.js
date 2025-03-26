73105f91eca25ce7e5f31cb0117b2a5fc5a4da5c
Randy Oest <oest@amazingrando.com>
2025-03-26 11:23:38 -0400

    wip

--- src/app/basic/page.js
+++ src/app/basic/page.js
@@ -1,6 +1,3 @@
-import clsx from "clsx";
-import BaruchLogo from "@/assets/logos/BaruchLogo";
-import Link from "next/link";
 import Banner from "@/components/banner";
 import Header from "@/components/header";
 import Program from "@/sections/program";
---
+++ src/components/utilityNav.js
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+
+const utilityNavItems = [
+  {
+    title: "Calendar",
+    url: "#"  // Add actual URLs as needed
+  },
+  {
+    title: "Directory",
+    url: "#"
+  },
+  {
+    title: "Library",
+    url: "#"
+  },
+  {
+    title: "President's Office",
+    url: "#"
+  },
+  {
+    title: "News Center",
+    url: "#"
+  },
+  {
+    title: "Technology",
+    url: "#"
+  },
+  {
+    title: "Index A-Z",
+    url: "#"
+  }
+];
+
+export default function UtilityNav() {
+  return (
+    <nav className="bg-primary-indigo py-1">
+      <ul className="flex items-center justify-end gap-4 mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        {utilityNavItems.map((item) => (
+          <li key={item.title}>
+            <a href={item.url} className={clsx("text-sm text-white/90 hover:text-white leading-none",
+              "after:content-[''] after:block after:max-w-0 after:-top-1 after:relative hover:after:max-w-full after:transition-all hover:after:duration-300 after:h-0.25 hover:after:bg-primary-sky"
+            )}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
