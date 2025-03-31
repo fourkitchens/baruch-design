cdd192906a147d7643056bedc3029e7d93a648b7
Randy Oest <oest@amazingrando.com>
2025-03-31 15:16:28 -0400

    wip

---
+++ src/app/basic/program/page.js
@@ -0,0 +1,15 @@
+import Banner from "@/components/banner";
+import Header from "@/components/header";
+import Program from "@/sections/program";
+import Footer from "@/components/footer";
+export default function Basic() {
+
+  return (
+    <div>
+      <Header/>
+      <Program/>
+      <Banner/>
+      <Footer/>
+    </div>
+  );
+}
