d74c9a4eca55f40da513076e7953ca85a7b1edc8
Randy Oest <oest@amazingrando.com>
2025-04-04 13:29:33 -0400

    wip

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,32 @@ import Search from "@/components/search";
 export default function Header() {
   return (
     <header className="bg-gradient-to-r from-primary-indigo via-primary-cuny-blue to-secondary-grape">
-        <div className="flex flex-row items-end justify-start pt-10 pb-8 max-width-wrapper">
-          <div>
-            <BaruchLogoHorizontal className="w-[300px] text-white" />
-            <p className="text-primary-sky font-field-gothic uppercase font-bold text-6xl tracking-wide leading-[50px]">Accelerate your ambition.</p>
-          </div>
-          <CunyLogoLong className="w-[150px] h-auto text-white ms-auto" />
+
+      <div className="flex flex-row items-end justify-start pt-10 pb-8 max-width-wrapper">
+        <div>
+          <BaruchLogoHorizontal className="w-[300px] text-white" />
+          <p className="text-primary-sky font-field-gothic uppercase font-bold text-6xl tracking-wide leading-[50px]">Accelerate your ambition.</p>
         </div>
         
-        <div className="border-t border-neutral-dove/20">
-          <div className="max-width-wrapper py-1 flex flex-row items-center justify-between">
-            <PrimaryNav />
-            <Search />
+        <div className="ms-auto flex flex-row items-center gap-8">
+          <div className="flex justify-end gap-4 py-2">
+            <a href="/apply" className="bg-primary-sky ring-1 ring-primary-sky hover:bg-white hover:ring-white text-primary-indigo px-6 py-2 font-bold rounded-sm transition-all">
+              Apply Now
+            </a>
+            <a href="/visit" className="bg-transparent ring-1 ring-neutral-dove/70 hover:bg-white hover:ring-white hover:text-primary-indigo text-white px-6 py-2 font-bold rounded-sm transition-colors">
+              Plan a Visit
+            </a>
           </div>
+          <CunyLogoLong className="w-[100px] h-auto text-white ms-auto" />
         </div>
+      </div>
+      
+      <div className="border-t border-neutral-dove/20">
+        <div className="max-width-wrapper py-1 flex flex-row items-center justify-between">
+          <PrimaryNav />
+          <Search />
+        </div>
+      </div>
     </header>
   );
 }
