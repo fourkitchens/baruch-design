97e8afa2848aeaa2fcb7f895d54c75542c992e16
Randy Oest <oest@amazingrando.com>
2025-04-08 13:04:02 -0400

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -25,10 +25,10 @@ export default function Home() {
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-bold">Basic page</h2>
             <ul className="text-lg flex flex-col gap-4">
+              <li><Link href="/basic/cabinet" className="text-white/90 link link-indigo-bg hover:text-white">Cabinet</Link></li>
               <li><Link href="/basic/news-list" className="text-white/90 link link-indigo-bg hover:text-white">News list</Link></li>
               <li><Link href="/basic" className="text-white/90 link link-indigo-bg hover:text-white">News article</Link></li>
               <li><Link href="/basic/provost" className="text-white/90 link link-indigo-bg hover:text-white">Accreditation</Link></li>
-              <li><Link href="/basic/cabinet" className="text-white/90 link link-indigo-bg hover:text-white">Cabinet</Link></li>
               <li><Link href="/basic/everything" className="text-white/90 link link-indigo-bg hover:text-white">Everything</Link></li>
             </ul>
           </div>
