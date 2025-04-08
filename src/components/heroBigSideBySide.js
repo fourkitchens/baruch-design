1c4cafdf9877e41186c535e1257763a1c18969b5
Randy Oest <oest@amazingrando.com>
2025-04-07 23:27:50 -0400

    wip

--- src/components/heroBigSideBySide.js
+++ src/components/heroBigSideBySide.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import clsx from "clsx";
 
-export default function HeroBigSideBySide({ image, title, content }) {
+export default function HeroBigSideBySide({ image, title, content, date }) {
   return (
     <div className="relative bg-secondary-tangerine text-white">
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
@@ -10,10 +10,11 @@ export default function HeroBigSideBySide({ image, title, content }) {
           "mx-auto max-w-2xl",
           "py-8 px-6",
           "md:py-16",
-          "lg:col-span-7 lg:pb-32 lg:pt-10 lg:px-0 lg:max-w-lg lg:mx-0",
+          "lg:col-span-7 lg:pb-32 lg:pt-10 lg:px-0 lg:max-w-lg lg:mx-0 lg:mt-24",
           "xl:col-span-6",
         )}>
-          <h1 className="lg:mt-24 text-pretty text-[56px] lg:text-[86px] leading-none uppercase font-normal tracking-tight font-league-gothic text-white">
+          {date && <p className="text-white text-base font-medium mb-2">{date}</p>}
+          <h1 className="text-pretty text-[56px] lg:text-[86px] leading-none uppercase font-normal tracking-tight font-league-gothic text-white">
             {title}
           </h1>
           <p className="mt-4 lg:mt-8 text-pretty text-base lg:text-lg font-medium text-white sm:text-xl/8">
