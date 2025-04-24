722566522dc10e0ded5ab7036fead093fddaae04
Randy Oest <oest@amazingrando.com>
2025-04-24 15:55:13 -0400

    wip

---
+++ src/stories/SlidingCards.stories.js
@@ -0,0 +1,10 @@
+import SlidingCards from "../components/slidingCards";
+
+export default {
+  title: 'Components/Sliding Cards',
+  component: SlidingCards,
+};
+
+export const SlidingCards1 = {
+  name: 'Sliding Cards',
+};
