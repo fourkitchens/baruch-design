39fc92528c493feb1f3e99159e4622d3bdf319ca
Randy Oest <oest@amazingrando.com>
2025-04-24 23:30:52 -0400

    wip

--- src/stories/LandingPage.stories.js
+++ src/stories/LandingPage.stories.js
@@ -3,13 +3,6 @@ import Zicklin from "@/templates/zicklin";
 export default {
   title: 'Templates/Landing Pages', 
   component: Zicklin,
-  decorators: [
-    (Story) => (
-      <div className="min-h-[700vh]">
-        <Story />
-      </div>
-    ),
-  ],
 };
 
 export const LandingPageZicklin = {
