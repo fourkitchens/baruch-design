cfbfa349298387155ea8ff31189aa7070e5a7942
Randy Oest <oest@amazingrando.com>
2025-04-28 16:21:54 -0400

    wip

--- src/components/primaryNavMobile.js
+++ src/components/primaryNavMobile.js
@@ -21,39 +21,27 @@ export default function PrimaryNavMobile({className}) {
         <Bars3Icon className="size-9" aria-hidden="true" />
       </button>
     
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gradient-to-br from-primary-indigo to-primary-cuny-blue text-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-start justify-between">
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden relative z-50">
+        {/* <div className="fixed inset-0 z-10" /> */}
+
+        <DialogPanel className="fixed inset-0 bg-primary-indigo text-white">
+          
+          <header className="flex flex-row items-center justify-start py-7 max-width-wrapper">
             <div className="space-y-2">
-              <BaruchLogoHorizontal className="w-[200px] h-auto lg:w-[300px] text-white mt-1" />
-              <AccelerateAmbition className="text-primary-sky lg:w-[250px] h-auto" />
+              <BaruchLogoHorizontal className="w-[200px] h-auto lg:w-[300px] text-white" />
+              <AccelerateAmbition className="text-secondary-chartreuse lg:w-[250px] h-auto" />
             </div>
+
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-white"
+              className="-m-2.5 ml-auto rounded-md p-2.5 text-white"
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <XMarkIcon aria-hidden="true" className="size-9" />
             </button>
-          </div>
+          </header>
           
-          <div className="mt-6 flex flex-col gap-y-2">
-            <div className="space-y-2 py-6">
-              {mainNavLinks.map((item) => (
-                <Disclosure key={item.name}>
-                  <DisclosureButton className="text-xl font-bold text-white/90 hover:text-white">
-                    {item.title}
-                    <ChevronDownIcon aria-hidden="true" className="size-5" />
-                  </DisclosureButton>
-                  <DisclosurePanel className="text-gray-500">
-                    Yes! You can purchase a license that you can share with your entire team.
-                  </DisclosurePanel>
-                </Disclosure>
-              ))}
-            </div>
-          </div>
         </DialogPanel>
       </Dialog>
     </div>
