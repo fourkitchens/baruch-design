a919591e93b18759ede697d3f51aec8193e5bf85
Randy Oest <oest@amazingrando.com>
2025-05-12 14:48:13 -0400

    Section banner

--- src/components/sectionBanner.js
+++ src/components/sectionBanner.js
@@ -1,9 +1,9 @@
 const SectionBanner = ({organizationalDescriptor, title}) => {
   return (
-    <div className="bg-white text-body py-3 border-b border-neutral-dove">
+    <div className="bg-white text-body pt-3 pb-2 lg:pt-6 lg:pb-5 border-b border-neutral-dove">
       <div className="max-width-wrapper ">
-        <p className="text-[100px] leading-[100px] uppercase font-bold font-field-gothic flex flex-row gap-2">
-          <span className="text-body font-normal">{organizationalDescriptor}</span>
+        <p className="text-3xl lg:text-5xl uppercase font-bold font-sans-xcondensed leading-none">
+          <span className="text-body font-medium">{organizationalDescriptor}</span>{' '}
           <span className="text-heading">{title}</span>
         </p>
       </div>
