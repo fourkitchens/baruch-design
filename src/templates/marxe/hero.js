09a608e3b3fbc0952f5370b85a3c98b2f9fe920b
Randy Oest <oest@amazingrando.com>
2025-06-04 13:27:56 -0400

    wip

--- src/templates/marxe/hero.js
+++ src/templates/marxe/hero.js
@@ -1,7 +1,7 @@
 "use client";
 
 import clsx from "clsx";
-import bgImage from '@/assets/images/marxe/IMG_5176.jpg'
+import bgImage from '@/assets/images/marxe/IMG_5176 copy.jpg'
 import Image from "next/image";
 
 export default function Hero() {
